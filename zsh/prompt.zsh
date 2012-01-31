0dd80946b0279645c32c38fa7a4fd265921ca3e9
Yura Zatsepin <yura.zatsepin@gmail.com>
2012-01-31 17:50:31 +0600

    sm fun check

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -49,6 +49,19 @@ rvm_prompt(){
   fi
 }
 
+env(){
+  if $(which virtualenv &> /dev/null) 
+  then
+    if [ $VIRTUAL_ENV ]; then
+      echo "<%{$fg_bold[yellow]%}venv:${VIRTUAL_ENV##*/}%{$reset_color%}>" # last folder's name in a directory path
+    else
+      echo ""
+    fi
+    else
+      echo ""
+  fi
+}
+
 # This keeps the number of todos always available the right hand side of my
 # command line. I filter it to only count those tagged as "+next", so it's more
 # of a motivation to clear out the list.
@@ -68,13 +81,46 @@ todo(){
   fi
 }
 
+# Determine the time since last commit. If branch is clean,
+# # use a neutral color, otherwise colors will vary according to time.
+git_time_since_commit() {
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+      #
+      if [[ $(git log 2>&1 > /dev/null | grep -c "^fatal: bad default revision") == 0 ]]; then
+          #Get the last commit.
+          last_commit=`git log --pretty=format:'%at' -1 2> /dev/null`
+          now=`date +%s`
+          seconds_since_last_commit=$((now-last_commit))
+          
+          #Totals
+          MINUTES=$((seconds_since_last_commit / 60))
+          HOURS=$((seconds_since_last_commit/3600))
+
+          # Sub-hours and sub-minutes
+          DAYS=$((seconds_since_last_commit / 86400))
+          SUB_HOURS=$((HOURS % 24))
+          SUB_MINUTES=$((MINUTES % 60))
+
+          if [ "$HOURS" -gt 24 ]; then
+            echo "${DAYS} d ${SUB_HOURS} h ${SUB_MINUTES} m %{$reset_color%}|"
+          elif [ "$MINUTES" -gt 60 ]; then
+            echo "${HOURS} h ${SUB_MINUTES} m%{$reset_color%}|"
+          else
+            echo "${MINUTES}m%{$reset_color%}|"
+          fi
+      else
+          echo ""
+      fi 
+  fi
+}
+
 directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rvm_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(env) in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
+    export RPROMPT="%{$fg_bold[cyan]%}$(git_time_since_commit)$(todo)%{$reset_color%}"
 }
 
 precmd() {
